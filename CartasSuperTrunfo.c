13dd7ea6af860be86d68207727aae4d4b67840a4
rubensra <30912886+rubensra@users.noreply.github.com>
2026-03-06 00:28:53 +0000

    Cadastrando as Variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,14 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char Estado_Carta1, Estado_Carta2;
+  char Cod_Carta1[3], Cod_Carta2[3];
+  char NomeCidade_Carta1[50], NomeCidade_Carta2[50];
+  int Populacao_Carta1, Populacao_Carta2;
+  float Area_Carta1, Area_Carta2;
+  float PIB_Carta1, PIB_Carta2;
+  int Num_PontosTuristicos_Carta1, Num_PontosTuristicos_Carta2;
+
 
   // Área para entrada de dados
 
